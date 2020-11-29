603b739bbab4b3c48f29fa12f1c0f138dec3adcd
severinsimmler <severin.simmler@posteo.de>
2020-11-29 15:03:34 +0100

    chore: add test

---
+++ tests/test_logging.py
@@ -0,0 +1,14 @@
+from logging import Formatter
+from chaine import logging
+
+
+def test_logger(caplog):
+    logger = logging.Logger("test")
+
+    assert logger.name == "test"
+    assert isinstance(logger.formatter, Formatter)
+
+    logger.log_level = logger.DEBUG
+    assert logger.log_level == logger.DEBUG
+    logger.log_level = logger.WARNING
+    assert logger.log_level == logger.WARNING

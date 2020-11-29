b8d55bf4b33f6abb1bdf2a0b98fda3986b6fbf47
severinsimmler <severin.simmler@posteo.de>
2020-11-29 14:57:59 +0100

    chore: add docstring

--- chaine/data.py
+++ chaine/data.py
@@ -6,9 +6,8 @@ This module provides basic data structures
 """
 
 from dataclasses import dataclass
-from os import stat
 
-from chaine.typing import FeatureGenerator, List, TokenGenerator, Iterable
+from chaine.typing import FeatureGenerator, List, TokenGenerator
 
 
 @dataclass
@@ -58,28 +57,47 @@ class Sequence:
     tokens: List[Token]
 
     def __getitem__(self, index: int) -> Token:
+        """Get token of the sequence by index"""
         return self.tokens[index]
 
     def __iter__(self) -> TokenGenerator:
+        """Iterate ove the tokens of the sequence"""
         for token in self.tokens:
             yield token
 
     def __len__(self) -> int:
+        """Number of tokens in the sequence"""
         return len(self.tokens)
 
     def __repr__(self) -> str:
+        """Representation of the sequence"""
         return f"<Sequence: {self.tokens}>"
 
     def __str__(self) -> str:
-        # TODO Overwrite for detokenization
+        """String representation of the sequence
+
+        Note
+        ----
+        Overwrite this method for custom detokenization
+        """
         return " ".join(token.text for token in self.tokens)
 
     @property
     def indices(self) -> List[int]:
+        """Indices of the token sequence"""
         return [token.index for token in self.tokens]
 
     def featurize(self) -> FeatureGenerator:
-        # TODO Overwrite for proper feature extraction
+        """Extract features from tokens of the sequence
+
+        Note
+        ----
+        Overwrite this method for custom feature extraction – which is generally
+        recommended as the default features may result in very low accuracy.
+
+        One token is represented as a set of strings, each string is a unique feature,
+        e.g. the string representation of the current token.
+        """
         for token in self.tokens:
             features = {f"token.lower()={token.lower()}",
                 f"token.is_upper()={token.is_upper}",

c59f13b21ecdcfc7223fa0dcadad01b186f0d7d3
severinsimmler <severin.simmler@posteo.de>
2020-11-29 15:24:43 +0100

    chore: add test

---
+++ tests/test_core.py
@@ -0,0 +1,17 @@
+from chaine import core
+from chaine.data import Token, Sequence
+from chaine.model import CRF
+
+
+def test_train():
+    tokens = [Token(0, "Foo"), Token(1, "bar")]
+    sequence = Sequence(tokens)
+
+    dataset = [sequence]
+    labels = [["O", "O"]]
+
+    crf = core.train(dataset, labels)
+
+    assert isinstance(crf, CRF)
+    assert crf.labels == {"O"}
+    assert crf.predict(sequence.featurize()) == ["O", "O"]

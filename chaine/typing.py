b3f17046e9f108e74e65a977b44b46b36299ec71
severinsimmler <severin.simmler@posteo.de>
2020-11-29 21:41:33 +0100

    chore: docstring

--- chaine/typing.py
+++ chaine/typing.py
@@ -1,3 +1,10 @@
+"""
+chaine.typing
+~~~~~~~~~~~~~
+
+A collection of type hints
+"""
+
 from typing import List, Optional, Generator, Iterable
 
 FeatureGenerator = Generator[List[str], None, None]

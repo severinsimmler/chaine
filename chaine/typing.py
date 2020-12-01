4e8b17bbe11232a7bcbb3cbd1274ee3cad1268d7
severinsimmler <severin.simmler@posteo.de>
2020-12-01 23:21:10 +0100

    chore: type hints

--- chaine/typing.py
+++ chaine/typing.py
@@ -5,8 +5,12 @@ chaine.typing
 A collection of type hints
 """
 
-from typing import List, Optional, Generator, Iterable, Union, Any
+from pathlib import Path as _Path
+from typing import List, Optional, Generator, Iterable, Union, Any, Set
 
 FeatureGenerator = Generator[List[str], None, None]
 TokenGenerator = Generator["Token", None, None]
 Labels = Iterable[str]
+Dataset = Iterable[Iterable[str]]
+Path = Union[_Path, str]
+Sequence = List[Set[str]]

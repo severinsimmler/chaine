bfad01a9ae1d93e4aa8978765ded3162decf3bcf
severinsimmler <severin.simmler@posteo.de>
2020-12-06 22:54:03 +0100

    chore: cleanup

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,3 @@
-import pytest
-
 from chaine import data
 
 
@@ -13,46 +11,3 @@ def test_token():
     assert token.is_lower == False
     assert token.is_title == True
     assert token.is_upper == False
-
-
-def test_token_sequence():
-    tokens = [data.Token(0, "Foo"), data.Token(1, "Bar")]
-    sequence = data.TokenSequence(tokens)
-
-    assert sequence.items == tokens
-    assert sequence[0] == tokens[0]
-    assert sequence[1] == tokens[1]
-
-    with pytest.raises(IndexError):
-        sequence[2]
-
-    assert list(sequence) == tokens
-    assert len(sequence) == 2
-    assert repr(sequence) == "<TokenSequence: [<Token 0: Foo>, <Token 1: Bar>]>"
-    assert str(sequence) == "Foo Bar"
-    assert sequence.indices == [0, 1]
-
-    strings = ["Foo", "Bar"]
-    sequence = data.TokenSequence(strings)
-    assert isinstance(sequence[0], str)
-
-
-def test_label_sequence():
-    labels = ["B-PER", "I-PER", "O"]
-    sequence = data.LabelSequence(labels)
-
-    assert sequence.items == labels
-    assert sequence[0] == labels[0]
-    assert sequence[1] == labels[1]
-    assert sequence[2] == labels[2]
-
-    with pytest.raises(IndexError):
-        sequence[3]
-
-    assert list(sequence) == labels
-    assert len(sequence) == 3
-    assert repr(sequence) == "<LabelSequence: ['B-PER', 'I-PER', 'O']>"
-    assert str(sequence) == "B-PER, I-PER, O"
-    assert sequence == data.LabelSequence(labels)
-    assert sequence != data.LabelSequence(["B-ORG", "I-ORG", "O"])
-    assert sequence.distinct == set(labels)

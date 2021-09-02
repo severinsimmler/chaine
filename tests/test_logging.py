ee60207cf15278602b1dbd63b2f105eae823561a
severinsimmler <s.simmler@snapaddy.com>
2021-09-02 12:48:32 +0200

    fix: typo

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,15 +1,13 @@
-from logging import Formatter
-
 from chaine import logging
+from chaine.logging import DEBUG, WARNING
 
 
 def test_logger():
     logger = logging.Logger("test")
 
     assert logger.name == "test"
-    assert isinstance(logger.formatter, Formatter)
 
-    logger.log_level = logger.DEBUG
-    assert logger.log_level == logger.DEBUG
-    logger.log_level = logger.WARNING
-    assert logger.log_level == logger.WARNING
+    logger.level = DEBUG
+    assert logger.level == DEBUG
+    logger.level = WARNING
+    assert logger.level == WARNING

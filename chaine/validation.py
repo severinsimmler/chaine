09da67251f816c9d6ab1cb17c5692bafdd7f077f
severinsimmler <severin.simmler@posteo.de>
2021-12-27 00:38:49 +0100

    fix: #52

---
+++ chaine/validation.py
@@ -0,0 +1,36 @@
+from chaine.typing import Sequence
+
+# supported data types for feature values
+TYPES = (str, int, float, bool)
+
+
+def is_valid_sequence(sequence: Sequence) -> bool:
+    """Check if the given sequence has valid input format.
+
+    Parameters
+    ----------
+    sequence : Sequence
+        Sequence to validate.
+
+    Returns
+    -------
+    bool
+        True if sequence is valid, False otherwise.
+    """
+    return isinstance(sequence, list) and all(is_valid_token(token) for token in sequence)
+
+
+def is_valid_token(token: dict) -> bool:
+    """Check if the given token has valid input format.
+
+    Parameters
+    ----------
+    token : dict
+        Token to validate.
+
+    Returns
+    -------
+    bool
+        True if sequence is valid, False otherwise.
+    """
+    return isinstance(token, dict) and all(isinstance(value, TYPES) for value in token.values())

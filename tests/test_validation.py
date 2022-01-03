8cb8268fc71b489bc7e052f7eb595986b9a2fb48
severinsimmler <s.simmler@snapaddy.com>
2022-01-03 16:49:22 +0100

    chore: unit test

---
+++ tests/test_validation.py
@@ -0,0 +1,21 @@
+from chaine import validation
+
+
+def test_is_valid_sequence():
+    assert validation.is_valid_sequence([{"int": 0, "str": "a", "float": 1.0, "bool": True}])
+
+    assert not validation.is_valid_sequence([[{"int": 0, "str": "a", "float": 1.0, "bool": True}]])
+    assert not validation.is_valid_sequence([{"int": 0, "str": ["a"], "float": 1.0}])
+    assert not validation.is_valid_sequence(["foo", "bar"])
+    assert not validation.is_valid_sequence([["foo", "bar"]])
+    assert not validation.is_valid_sequence({"int": 0, "str": "a", "float": 1.0, "bool": True})
+    assert not validation.is_valid_sequence(({"int": 0} for _ in range(5))
+    )
+
+
+def test_is_valid_token():
+    assert validation.is_valid_token({0: 0, "int": 0, "str": "a", "float": 1.0, "bool": True})
+
+    assert not validation.is_valid_token({"int": [0], "str": "a", "float": 1.0, "bool": True})
+    assert not validation.is_valid_token({"int": 0, "str": "a", "float": 1.0, "dict": {}})
+    assert not validation.is_valid_token("foo")

66c5688e956f54804e2d4b0b423ef6f4fd9f3618
severinsimmler <s.simmler@snapaddy.com>
2026-01-07 14:27:09 +0100

    feat: add build script

---
+++ setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from Cython.Build import cythonize
+
+
+SOURCES = [
+    "chaine/_core/crf.pyx",
+    "chaine/_core/crfsuite/lib/cqdb/src/cqdb.c",
+    "chaine/_core/crfsuite/lib/cqdb/src/lookup3.c",
+    "chaine/_core/crfsuite/lib/crf/src/crf1d_context.c",
+    "chaine/_core/crfsuite/lib/crf/src/crf1d_encode.c",
+    "chaine/_core/crfsuite/lib/crf/src/crf1d_feature.c",
+    "chaine/_core/crfsuite/lib/crf/src/crf1d_model.c",
+    "chaine/_core/crfsuite/lib/crf/src/crf1d_tag.c",
+    "chaine/_core/crfsuite/lib/crf/src/crfsuite.c",
+    "chaine/_core/crfsuite/lib/crf/src/crfsuite_train.c",
+    "chaine/_core/crfsuite/lib/crf/src/dataset.c",
+    "chaine/_core/crfsuite/lib/crf/src/dictionary.c",
+    "chaine/_core/crfsuite/lib/crf/src/holdout.c",
+    "chaine/_core/crfsuite/lib/crf/src/json.c",
+    "chaine/_core/crfsuite/lib/crf/src/logging.c",
+    "chaine/_core/crfsuite/lib/crf/src/params.c",
+    "chaine/_core/crfsuite/lib/crf/src/quark.c",
+    "chaine/_core/crfsuite/lib/crf/src/rumavl.c",
+    "chaine/_core/crfsuite/lib/crf/src/train_arow.c",
+    "chaine/_core/crfsuite/lib/crf/src/train_averaged_perceptron.c",
+    "chaine/_core/crfsuite/lib/crf/src/train_l2sgd.c",
+    "chaine/_core/crfsuite/lib/crf/src/train_lbfgs.c",
+    "chaine/_core/crfsuite/lib/crf/src/train_passive_aggressive.c",
+    "chaine/_core/crfsuite/swig/crfsuite.cpp",
+    "chaine/_core/liblbfgs/lib/lbfgs.c",
+    "chaine/_core/trainer_wrapper.cpp",
+]
+
+INCLUDE_DIRS = [
+    "chaine/_core/crfsuite/include/",
+    "chaine/_core/crfsuite/lib/cqdb/include",
+    "chaine/_core/liblbfgs/include",
+    "chaine/_core",
+]
+
+EXTENSION = Extension(
+    "chaine._core.crf",
+    language="c++",
+    include_dirs=INCLUDE_DIRS,
+    sources=SOURCES,
+)
+
+
+class ExtensionBuilder(build_ext):
+    def build_extensions(self):
+        c = self.compiler
+        _compile = c._compile
+
+        def c_compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
+            if src.endswith(".c"):
+                cc_args += ["-std=c99", "-D_POSIX_C_SOURCE=200112L"]
+            return _compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+
+        c._compile = c_compile
+        build_ext.build_extensions(self)
+
+
+setup(
+    cmdclass={"build_ext": ExtensionBuilder},
+    ext_modules=cythonize([EXTENSION]),
+)
